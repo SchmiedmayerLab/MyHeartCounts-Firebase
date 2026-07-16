d948cf1e897d87f1bb929c017ed1ec4f5a8b9bdf
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-07-16 10:22:25 +0200

    add more secrets

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -7,8 +7,12 @@ import { defineSecret } from "firebase-functions/params";
 
 enum SecretKey {
   OPENAI_API_KEY = "OPENAI_API_KEY",
+  SMTP_HOST = "SMTP_HOST",
+  SMTP_PORT = "SMTP_PORT",
   SMTP_USERNAME = "SMTP_USERNAME",
   SMTP_PASSWORD = "SMTP_PASSWORD",
+  FEEDBACK_SENDER_EMAIL = "FEEDBACK_SENDER_EMAIL",
+  FEEDBACK_COORDINATOR_EMAIL = "FEEDBACK_COORDINATOR_EMAIL",
 }
 
 const openaiApiKey = defineSecret(SecretKey.OPENAI_API_KEY);
@@ -19,13 +23,38 @@ export const getOpenaiSecretKeys = (): string[] => [SecretKey.OPENAI_API_KEY];
 
 export const openaiApiKeyParam: ReturnType<typeof defineSecret> = openaiApiKey;
 
+const smtpHost = defineSecret(SecretKey.SMTP_HOST);
+const smtpPort = defineSecret(SecretKey.SMTP_PORT);
 const smtpUsername = defineSecret(SecretKey.SMTP_USERNAME);
 const smtpPassword = defineSecret(SecretKey.SMTP_PASSWORD);
+const feedbackSenderEmail = defineSecret(SecretKey.FEEDBACK_SENDER_EMAIL);
+const feedbackCoordinatorEmail = defineSecret(
+  SecretKey.FEEDBACK_COORDINATOR_EMAIL,
+);
+
+export const getSmtpHost = (): string => smtpHost.value();
+
+export const getSmtpPort = (): string => smtpPort.value();
 
 export const getSmtpUsername = (): string => smtpUsername.value();
 
 export const getSmtpPassword = (): string => smtpPassword.value();
 
+export const getFeedbackSenderEmail = (): string => feedbackSenderEmail.value();
+
+export const getFeedbackCoordinatorEmail = (): string =>
+  feedbackCoordinatorEmail.value();
+
 export const smtpUsernameParam: ReturnType<typeof defineSecret> = smtpUsername;
 
 export const smtpPasswordParam: ReturnType<typeof defineSecret> = smtpPassword;
+
+export const feedbackEmailSecretParams: Array<ReturnType<typeof defineSecret>> =
+  [
+    smtpHost,
+    smtpPort,
+    smtpUsername,
+    smtpPassword,
+    feedbackSenderEmail,
+    feedbackCoordinatorEmail,
+  ];
