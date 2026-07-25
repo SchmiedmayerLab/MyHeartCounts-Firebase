689e62d4775888320b8d03c1859245d5c832e243
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-07-25 16:40:13 -0700

    Migrate repository references to SchmiedmayerLab

--- functions/src/functions/planNudges.ts
+++ functions/src/functions/planNudges.ts
@@ -111,7 +111,7 @@ export class NudgeService {
   // Methods
 
   private mapComorbidityKeyToDisease(key: string): Disease | null {
-    // These are defined for iOS in https://github.com/StanfordBDHG/MyHeartCounts-iOS/blob/main/MyHeartCounts/Account/Demographics/Comorbidities.swift.
+    // These are defined for iOS in https://github.com/SchmiedmayerLab/MyHeartCounts-iOS/blob/main/MyHeartCounts/Account/Demographics/Comorbidities.swift.
     switch (key) {
       case "heartFailure":
         return Disease.HEART_FAILURE;
